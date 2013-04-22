5622f87c2d1aea104d418a86ba5f62c6bbb0c3d3
Elad Rom <webdev@eladrom.org>
2013-04-22 16:30:32 +0300

    adding dry run script for bash

---
+++ bin/dry-run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# dry is a variable that indicates whether we want to do dry run or not
+# Currently dry run is enabled. To disable it, set the dry variable to any other string
+# Note: define the dry variable above the execute function below so
+# that its available in the execute function
+dry="y"
+
+# Function: execute
+function execute(){
+        # Irrespective of whether dry run is enabled or not, we display
+        # the command on the screen
+	echo "COMMAND: ${@}"
+
+        # if dry run is enabled then simply return
+	if [ $dry == "y" ]; then
+		return 0
+	fi
+
+        # if dry run is disabled, then execute the command
+	eval "$@"
+}
+
+# Now just append execute to all your commands
+# This will cause them to print irrespective of whether we are doing any dry run or not
+# Commands are only executed if dry run is disabled
